6f9bfc49d00f7922457d122a6da690ca8ebf25bf
Arnab <arnabkumar.coding@gmail.com>
2022-05-08 17:35:07 +0530

    Footer using JS

---
+++ footer.js
@@ -0,0 +1,61 @@
+document.getElementById("footer").innerHTML=`
+<div class="footer-dark">
+    <footer>
+       
+        <ul class="CU-OB">
+            <li class="CU"><a href="#"></a>Contact us</li>
+            <li class="OB"><a href="#"></a>Online Booking</li>
+            </ul>
+       
+       
+        <section class="H3">
+           
+            <h3>
+                Contact us on
+                </h3>
+            <div class="wrapper">
+                <div class="button">
+                    <a href="https://www.youtube.com/">
+                        <div class="icon">
+                            <i class="fab fa-facebook"></i>
+                            </div>
+                        <span>Facebook</span>
+                        </a>
+                   
+                    </div>
+                <div class="button">
+                    <a href="https://www.youtube.com/">
+                        <div class="icon">
+                            <i class="fab fa-linkedin"></i>
+                            </div>
+                        <span>LinkedIn</span>
+                        </a>
+                    </div>
+                <div class="button">
+                    <a href="https://www.youtube.com/">
+                        <div class="icon">
+                            <i class="fab fa-twitter"></i>
+                            </div>
+                        <span>Twitter</span>
+                        </a>
+                   
+                    </div>
+                <div class="button">
+                    <a href="https://www.youtube.com/">
+                        <div class="icon">
+                            <i class="fab fa-instagram"></i>
+                            </div>
+                        <span>Instagram</span>
+                        </a>
+                    </div>
+                </div>
+            <section class="DOWN">
+                <div class="copyright" <p>© Company 2022 all reserved</p>
+                    <h3 class="mail"><a href="#">Diporbill.nits@gmail.com</a></h3>
+                    </div>
+                </section>
+            </section>
+       
+       </div>
+</footer>
+`
