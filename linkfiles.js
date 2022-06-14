d15e609860cf3693b8d227235a634c700d1d4d15
Arnab <arnabkumar.coding@gmail.com>
2022-06-14 15:13:12 +0530

    All Bugs Fixed

--- linkfiles.js
+++ linkfiles.js
@@ -8,6 +8,7 @@ document.write(`
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DiporBill</title>
     <!-- Stylesheets -->
+    <link href="Home.css" rel="stylesheet">
     <link href="home.css" rel="stylesheet">
     <link href="AboutUs.css" rel="stylesheet">
     <link href="footer.css" rel="stylesheet">
@@ -17,6 +18,7 @@ document.write(`
     <link href="navbar.css" rel="stylesheet">
     <link href="Flashcard.css" rel="stylesheet">
     <link rel="stylesheet" href="Tour guide.css">
+    <link href="gallery.css" rel="stylesheet">
     
     <!-- icons and APIs -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
@@ -28,18 +30,18 @@ document.write(`
 <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Tapestry&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps&display=swap" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200&family=Hahmlet:wght@200&family=Koulen&family=Poppins:wght@100&display=swap" rel="stylesheet">
+
+<!-- icons and APIs -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
 </head>
 
 <body>
     <!-- Headings-->
-    <div class="head">
-
-        
-            <!-- Colours-->
-        
-        <div class="nav1"><!-- Shade 1-->
-        <div class="nav1"><h1>Dipor Bill</h1>
-        <button id="nav-button"></button></div></div>
+    
         `
 );
 
