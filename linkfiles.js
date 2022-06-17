91b0b89726e46131ef250a7ba0fc8712e7c483df
Mrityunjoy01 <97493554+Mrityunjoy01@users.noreply.github.com>
2022-06-17 23:13:40 +0530

    spelling

--- linkfiles.js
+++ linkfiles.js
@@ -6,7 +6,7 @@ document.write(`
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DiporBill</title>
+    <title>DeeporBill</title>
     <!-- Stylesheets -->
     <link href="Home.css" rel="stylesheet">
     <link href="home.css" rel="stylesheet">
